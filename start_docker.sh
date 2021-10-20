4fe4be4cc13579fe6bcf56d84b6c3a960ef54b46
Daniel Derkacz <mail@daniel-derkacz.de>
2021-10-20 19:38:03 +0200

    start docker script

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,12 @@
+ROOT_DIR=/home/daniel/projects/idp
+DATASET_DIR=$ROOT_DIR/dataset/
+CODE_DIR=$ROOT_DIR/One-Thing-One-Click
+
+docker build -t derkaczda/one-thing-one-click .
+
 docker run -it --rm --privileged --gpus all -v /dev:/dev \
-    -v /home/daniel/projects/idp/dataset/scannet:/data \
-    -v /home/daniel/projects/idp/dataset/model:/model \
-    otoc python3 test.py --config config/pointgroup_run1_scannet.yaml --pretrain /model/pointgroup_run1_scannet-000001250.pth
+    -v $DATASET_DIR/scannet:/data \
+    -v $DATASET_DIR/model:/model \
+    -v $CODE_DIR/3D-U-Net/config/pointgroup_run1_scannet.yaml:/otoc/3D-U-Net/config/pointgroup_run1_scannet.yaml \
+    -v $CODE_DIR/experiments:/otoc/3D-U-Net/exp \
+    derkaczda/one-thing-one-click python3 train.py --config config/pointgroup_run1_scannet.yaml #--pretrain /model/pointgroup_run1_scannet-000000000.pth
