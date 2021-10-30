8dce2b6ee28117ba153f56f205989bc3174e8a49
Daniel Derkacz-Bogner <mail@daniel-derkacz.de>
2021-10-30 10:16:54 +0200

    added bash script to start docker env

---
+++ start_docker.sh
@@ -0,0 +1,22 @@
+U_NET_TEST="cd /otoc/3D-U-Net; python3 test.py \
+    --config config/pointgroup_run1_scannet.yaml \
+    --pretrain /model/pointgroup_run1_scannet-000001250.pth"
+RELATION_TEST="cd /otoc/relation; python3 test.py \
+    --config config/pointgroup_run1_scannet.yaml \
+    --pretrain /model/pointgroup_run1_scannet-000002891_weight.pth"
+MERGE_TEST="cd /otoc/merge; python3 test.py \
+    --config config/pointgroup_run1_scannet.yaml"
+
+CONTAINER_NAME="derkaczda/one-thing-one-click"
+CODE_ROOT=/home/daniel/projects/idp/One-Thing-One-Click
+
+docker run -it --rm --privileged --gpus all -v /dev:/dev \
+    -v scannet_dataset:/data \
+    -v otoc_pretrain_model:/model \
+    -v otoc_result:/result \
+    -v /home/daniel/projects/idp/One-Thing-One-Click/3D-U-Net/config/pointgroup_run1_scannet.yaml:/otoc/3D-U-Net/config/pointgroup_run1_scannet.yaml \
+    -v /home/daniel/projects/idp/One-Thing-One-Click/relation/config/pointgroup_run1_scannet.yaml:/otoc/relation/config/pointgroup_run1_scannet.yaml \
+    -v $CODE_ROOT/3D-U-Net/util/utils.py:/otoc/3D-U-Net/util/utils.py \
+    -v $CODE_ROOT/3D-U-Net/test.py:/otoc/3D-U-Net/test.py \
+    -v $CODE_ROOT/relation/test.py:/otoc/relation/test.py \
+    $CONTAINER_NAME bash -c "$RELATION_TEST"
